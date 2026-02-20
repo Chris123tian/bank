08cdabb52c65324aa9c86bc4d2de2df9a9108d48
Rescue Foundation <rescuefoundation4@gmail.com>
2026-02-20 09:43:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2097304882-94e70",
+  "appId": "1:296069437410:web:c2e2b14d5d8e58b0814bbb",
+  "apiKey": "AIzaSyAMkoYUiTwYbWPKCSJ8XJqyPyYaC6PsnEI",
+  "authDomain": "studio-2097304882-94e70.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "296069437410"
+};
